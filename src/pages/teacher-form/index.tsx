1967a318b647b12e812ebf496cd3e4e1a717f433
Leonardo Gurgel <lgurgel@id.uff.br>
2020-08-04 00:11:29 -0300

    WIP add teacher form

---
+++ src/pages/teacher-form/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import PageHeader from '../../components/page-header'
+import './styles.scss'
+
+export default function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title='Que incrível que você quer dar aulas.' />
+    </div>
+  )
+}
